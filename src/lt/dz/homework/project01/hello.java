00a88c622295e624940b27142c9f5267dbb8841d
DzMcHUB <79442208+DzMcHUB@users.noreply.github.com>
2021-03-28 23:07:32 +0300

    ini projects

---
+++ src/lt/dz/homework/project01/hello.java
@@ -0,0 +1,8 @@
+package lt.dz.homework.project01;
+
+public class hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
+}
---
+++ src/lt/dz/homework/project02/chessProblem.java
@@ -0,0 +1,11 @@
+package lt.dz.homework.project02;
+
+public class chessProblem {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 64; i++) {
+            int j = (int) Math.pow(2,i-1);
+            System.out.println("Ant " + i + " langelio " + ("padėti "+ j + " grūdai"));
+        }
+    }
+}
