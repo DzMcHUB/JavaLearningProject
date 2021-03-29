1bdd150d4b102478b0c2ffff200ba1bb3b75a024
DzMcHUB <79442208+DzMcHUB@users.noreply.github.com>
2021-03-29 23:08:55 +0300

    ini projects

--- src/lt/dz/homework/project02/chessProblem.java
+++ src/lt/dz/homework/project02/chessProblem.java
@@ -5,7 +5,7 @@ public class chessProblem {
     public static void main(String[] args) {
         for (int i = 1; i <= 64; i++) {
             int j = (int) Math.pow(2,i-1);
-            System.out.println("Ant " + i + " langelio " + ("padėti "+ j + " grūdus"));
+            System.out.println("Ant " + i + " langelio " + "padėti "+ j + " grūdus");
         }
     }
 }
