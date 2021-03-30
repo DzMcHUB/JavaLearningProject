8412a85340b7a5175e168e0781b691f552c8bf23
DzMcHUB <79442208+DzMcHUB@users.noreply.github.com>
2021-03-30 11:03:47 +0300

    ini projects

--- src/lt/dz/homework/project03/multiplicationTable.java
+++ src/lt/dz/homework/project03/multiplicationTable.java
@@ -3,11 +3,14 @@ package lt.dz.homework.project03;
 public class multiplicationTable {
 
     public static void main(String[] args) {
-        for (int i = 1; i <= 10; i++) {
+        for (int i = 1; i <= 9; i +=3) {
             for (int j = 1; j <= 10; j++){
-                System.out.println(i + "x" + j + " = " + i*j);
+                for (int n = 0; n<3; n++){
+                    System.out.print((i+n) + "x" + j + " = " + (i+n) * j + "   ");
+                }
+                System.out.println();
             }
-            System.out.println(" ");
+            System.out.println();
         }
 
     }
