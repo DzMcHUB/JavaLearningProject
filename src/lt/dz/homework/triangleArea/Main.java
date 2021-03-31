0931d51c65a896314f4ab817415266d937aa53e9
DzMcHUB <79442208+DzMcHUB@users.noreply.github.com>
2021-03-31 11:41:49 +0300

    triangelArea better if

--- src/lt/dz/homework/triangleArea/Main.java
+++ src/lt/dz/homework/triangleArea/Main.java
@@ -17,7 +17,7 @@ public class Main {
         double S=Math.sqrt(p*(p-a)*(p-b)*(p-c));
 
         if (a+b>c && a+c>b && b+c>a){
-            if(a==b && a==c && b==c){
+            if(a==b && b==c){
                 System.out.println("\n" + "Lygiakraštis trikampis");
             } else if (a==b || a==c || b==c) {
                 System.out.println("\n" + "Lygiašonis trikampis");
