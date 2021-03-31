36c29ecc0c04c01f733e3fff5929487be0b1239c
DzMcHUB <79442208+DzMcHUB@users.noreply.github.com>
2021-03-31 12:47:20 +0300

    arraySort correct solution

--- src/lt/dz/homework/arraySort/Main.java
+++ src/lt/dz/homework/arraySort/Main.java
@@ -1,17 +1,20 @@
 package lt.dz.homework.arraySort;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         int[] a = {-10,0,2,9,-5};
-        int[] b = {-9999999,-9999999,-9999999,-9999999,-9999999};
 
-        for ( int j = 0; j<5;j++){
-            for (int i = 0; i < 5; i++) {
-                if (a[i] > b[j] && a[i]!=b[0] && a[i]!=b[1] && a[i]!=b[2] && a[i]!=b[3] && a[i]!=b[4]) {
-                    b[j] = a[i];
+        for ( int j = 0; j < a.length; j++){
+            for (int i = 0; i < a.length; i++) {
+                if (a[j] > a[i]) {
+                    int temp = a[j];
+                    a[j] = a[i];
+                    a[i] = temp;
                 }
             }
         }
-        System.out.println(b[0] +" " + b[1] +" " + b[2] +" " + b[3] +" " + b[4] +" ");
+        System.out.println(Arrays.toString(a));
     }
 }
