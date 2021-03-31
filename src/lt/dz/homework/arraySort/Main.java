a27fb8a144f961b93b472377b95384fb02ef1a0c
DzMcHUB <79442208+DzMcHUB@users.noreply.github.com>
2021-03-31 13:29:38 +0300

    arraySort formating

--- src/lt/dz/homework/arraySort/Main.java
+++ src/lt/dz/homework/arraySort/Main.java
@@ -4,9 +4,9 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] a = {-10,0,2,9,-5};
+        int[] a = {-10, 0, 2, 9, -5};
 
-        for ( int j = 0; j < a.length; j++){
+        for (int j = 0; j < a.length; j++) {
             for (int i = 0; i < a.length; i++) {
                 if (a[j] > a[i]) {
                     int temp = a[j];
